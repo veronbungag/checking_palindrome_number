3b51b7d07d560808ec802dbba58c9cbcb4e43ee9
veronbungag <rveron.bungag@gmail.com>
2024-01-24 15:13:04 +0800

    checking string

--- checking_palindrome_number.py
+++ checking_palindrome_number.py
@@ -1,6 +1,7 @@
 #define a function for checking palindrome number
 def check_palindrome(number):
 #check the string is equal to its reverse then its palindrome number
+    return str(number) == str(number)[::-1]
 #convert number to string then user input number
 #check if the user input is palindrome number if it is then print
 #if not print not a palindrome number
